0f94afdbf6808db52ab7919f5dc467d2a7065d3d
LubsPants <luiza@etus.com.br>
2026-02-05 15:45:18 -0300

    Update cache version from v2 to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "calorias-pwa-v2";
+const CACHE = "calorias-pwa-v3";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./foods.json","./manifest.webmanifest"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
